62af4c97c1375625d30b0b89aaa9dc5582d9636d
YurijM <y-myagkov@mail.ru>
2023-03-08 19:56:57 +0300

    Разработка стартового шаблона

--- app/src/main/java/com/mu/ruslotto/ui/main/MainActivity.kt
+++ app/src/main/java/com/mu/ruslotto/ui/main/MainActivity.kt
@@ -2,10 +2,14 @@ package com.mu.ruslotto.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.mu.ruslotto.R
 import com.mu.ruslotto.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    //private var navController: NavController? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +18,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
+
+        val navController = (supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment).navController
+        val graph = navController.navInflater.inflate(R.navigation.main_graph)
+        graph.setStartDestination(R.id.issuesFragment)
+        navController.graph = graph
     }
 }
--- app/src/main/java/com/mu/ruslotto/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/mu/ruslotto/ui/splash/SplashFragment.kt
@@ -1,18 +1,24 @@
 package com.mu.ruslotto.ui.splash
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import com.mu.ruslotto.R
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.mu.ruslotto.ui.main.MainActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class SplashFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        lifecycleScope.launch(Dispatchers.IO) {
+            withContext(Dispatchers.Main) {
+                val intent = Intent(requireContext(), MainActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                startActivity(intent)
+            }
+        }
     }
 }
